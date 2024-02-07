e7ff447d3709b689fa4349a9a7c6a0132e114a77
Aryan <aryan.salmanpour@amd.com>
2024-02-07 10:49:01 -0500

    CMakeLists update

--- cmake/FindLibva.cmake
+++ cmake/FindLibva.cmake
@@ -1,19 +1,16 @@
 ################################################################################
-# 
-# MIT License
-# 
-# Copyright (c) 2023 -2024 Advanced Micro Devices, Inc.
-# 
+# Copyright (c) 2023 - 2024 Advanced Micro Devices, Inc.
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,14 +19,14 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 #
+################################################################################
 
-find_package(Libdrm REQUIRED)
-find_library(LIBVA_LIBRARY NAMES va)
-find_library(LIBVA_DRM_LIBRARY NAMES va-drm)
-find_path(LIBVA_INCLUDE_DIR NAMES va/va.h)
+find_library(LIBVA_LIBRARY NAMES va HINTS /opt/amdgpu/lib/x86_64-linux-gnu /opt/amdgpu/lib64)
+find_library(LIBVA_DRM_LIBRARY NAMES va-drm HINTS /opt/amdgpu/lib/x86_64-linux-gnu /opt/amdgpu/lib64)
+find_path(LIBVA_INCLUDE_DIR NAMES va/va.h PATHS /opt/amdgpu/include)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Libva DEFAULT_MSG LIBVA_INCLUDE_DIR LIBVA_LIBRARY LIBVA_DRM_LIBRARY)
+find_package_handle_standard_args(Libva DEFAULT_MSG LIBVA_INCLUDE_DIR LIBVA_LIBRARY)
 mark_as_advanced(LIBVA_INCLUDE_DIR LIBVA_LIBRARY LIBVA_DRM_LIBRARY)
 
 if(Libva_FOUND)
@@ -40,8 +37,13 @@ if(Libva_FOUND)
   endif()
   if(NOT TARGET Libva::va_drm)
     add_library(Libva::va_drm UNKNOWN IMPORTED)
-    target_link_libraries(Libva::va_drm INTERFACE Libdrm::drm)
     set_target_properties(Libva::va_drm PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${LIBVA_INCLUDE_DIR}"
-        IMPORTED_LOCATION "${LIBVA_DRM_LIBRARY}")
+      IMPORTED_LOCATION "${LIBVA_DRM_LIBRARY}")
+  endif()
+  message("-- ${White}Using Libva -- \n\tLibraries:${LIBVA_LIBRARY} \n\tIncludes:${LIBVA_INCLUDE_DIR}${ColourReset}")
+  message("-- ${White}Using Libva-drm -- \n\tLibraries:${LIBVA_DRM_LIBRARY}${ColourReset}")
+else()
+  if(Libva_FIND_REQUIRED)
+    message(FATAL_ERROR "{Red}FindLibva -- Libva NOT FOUND${ColourReset}")
   endif()
 endif()

ab8e9cdf015fd574cb309dbc9ca6fe8eec31c2e8
Kiriti Gowda <kiritigowda@gmail.com>
2025-03-19 11:20:22 -0500

    Update common.groovy

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -119,9 +119,9 @@ def runPackageCommand(platform, project) {
                 ${packageInfo} package/${osType}-rocjpeg-dev.${packageType}
                 ${packageInfo} package/${osType}-rocjpeg-test.${packageType}
                 ${packageInfo} package/${osType}-rocjpeg.${packageType}
+                ${packageInstall} package/${osType}-rocjpeg.${packageType}
                 ${packageInstall} package/${osType}-rocjpeg-dev.${packageType}
                 ${packageInstall} package/${osType}-rocjpeg-test.${packageType}
-                ${packageInstall} package/${osType}-rocjpeg.${packageType}
                 """
 
     platform.runCommand(this, command)

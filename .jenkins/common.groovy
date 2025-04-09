64d6c6ef3ed2fe4d4c48cfdfd790eccfcc8a6e07
Aryan Salmanpour <aryan.salmanpour@amd.com>
2025-04-09 11:33:25 -0400

    CI Update - Enhance testing for rocJPEG by using JPEG images with different chroma subsampling options

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -48,11 +48,14 @@ def runTestCommand (platform, project) {
                 cmake /opt/rocm/share/rocjpeg/samples/jpegDecode/
                 make -j8
                 LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/opt/rocm/lib${libLocation} ./jpegdecode -i /opt/rocm/share/rocjpeg/images/
+                echo rocjpeg additional tests
+                wget http://math-ci.amd.com/userContent/computer-vision/rocJPEG/jpeg_samples.zip
+                unzip jpeg_samples.zip
+                LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/opt/rocm/lib${libLocation} ./jpegdecode -i jpeg_samples
                 echo rocjpeg-test package verification
                 cd ../ && mkdir -p rocjpeg-test && cd rocjpeg-test
                 cmake /opt/rocm/share/rocjpeg/test/
                 LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/opt/rocm/lib${libLocation} ctest -VV --rerun-failed --output-on-failure
-                echo rocjpeg conformance tests
                 """
 
     platform.runCommand(this, command)
